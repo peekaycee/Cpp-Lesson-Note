34fb64d133672ee8ae6bd68939c9eeaa29c60051
peekaycee <kalukaluokomba@gmail.com>
2025-07-17 01:06:41 +0100

    Initial commit

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  string x = "Hello";
+  string changeLoginPassword = "Hello Greece";
+  
+
+  cout << changeLoginPassword << "\n";
+
+  return 0;
+
+}
