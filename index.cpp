75d0973ae0b003862c6a0473ed687bdd564368b7
peekaycee <kalukaluokomba@gmail.com>
2025-07-21 13:32:34 +0100

    Note Update -> 21-July-2025

--- index.cpp
+++ index.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-  
   string x = "Hello";
   string changeLoginPassword = "Hello Greece";
-  
-
   cout << changeLoginPassword << "\n";
-
+  // cout << cmath.Random();
   return 0;
-
 }
